4fb6ef5c35ef0de8905b4743b42e7ee2e05ed388
karishma <panchasarakarishma9@gmail.com>
2023-12-02 08:26:05 +0530

    updated insert API

--- CollegeProject/View/SignUpView.swift
+++ CollegeProject/View/SignUpView.swift
@@ -10,6 +10,49 @@ import PhotosUI
 
 struct SignUpView: View {
     
+    func sendDataToServer() {
+            guard let url = URL(string: "https://flashchatcollageproject.000webhostapp.com/Insert_API.php") else {
+                print("Invalid URL")
+                return
+            }
+
+            let parameters = [
+                "firstName": firstName,
+                "lastName": lastName,
+                "userName": username,
+                "contact": contactNumber,
+                "email": email,
+                "image": tempImage,
+                "password": password
+            ]
+
+            guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else {
+                print("Failed to serialize data")
+                return
+            }
+
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonData
+
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    return
+                }
+
+                if let data = data {
+                    // Process the response data here
+                    // Example: parse JSON response if the server returns JSON data
+                    if let responseString = String(data: data, encoding: .utf8) {
+                        print("Response: \(responseString)")
+                        // Handle the response accordingly in your app
+                    }
+                }
+            }.resume()
+        }
+    
     @State var firstName: String = ""
     @State var lastName: String = ""
     @State var username: String = ""
@@ -19,6 +62,7 @@ struct SignUpView: View {
     @State var email: String = ""
     @StateObject private var viewModel = ProfileViewModel()
     @State private var isImagePickerPresented = false
+    @State var tempImage = "person.circle"
     
     var body: some View {
         ZStack{
@@ -33,18 +77,20 @@ struct SignUpView: View {
                     Spacer()
                 }
                 
-                    Image(uiImage: viewModel.profilePicture)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .onTapGesture {
-                            isImagePickerPresented = true
-                        }
-
-                .sheet(isPresented: $isImagePickerPresented) {
-                    PhotoPickerView(image: $viewModel.profilePicture)
-                }
+//                    Image(uiImage: viewModel.profilePicture)
+//                        .resizable()
+//                        .scaledToFill()
+//                        .frame(width: 100, height: 100)
+//                        .clipShape(Circle())
+//                        .onTapGesture {
+//                            isImagePickerPresented = true
+//                        }
+//
+//                .sheet(isPresented: $isImagePickerPresented) {
+//                    PhotoPickerView(image: $viewModel.profilePicture)
+//                }
+                
+                TextFieldView(fieldData: $tempImage, placeholderText: "Enter Image...")
                 
                 Text("Enter Your Details")
                     .font(.title2)
@@ -64,6 +110,10 @@ struct SignUpView: View {
                     SecureFieldView(fieldData: $password, placeholderText: "Password...")
                     
                     SecureFieldView(fieldData: $reTypePassword, placeholderText: "Re - Type Your Password...")
+                    
+                    Button("send"){
+                        sendDataToServer()
+                    }
                     
                     
                     NavigationLink(destination: mainMsgView(), label: {
